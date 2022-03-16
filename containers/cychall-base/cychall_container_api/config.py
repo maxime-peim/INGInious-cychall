5b40fa539b60b07a3d91d0ecc8f543224cc03f12
Maxime Peim <maxime.peim@gmail.com>
2022-03-16 01:47:08 +0100

    Working step configuration, to improve

--- containers/cychall-base/cychall_container_api/config.py
+++ containers/cychall-base/cychall_container_api/config.py
@@ -1,36 +1,27 @@
-# -*- coding: utf-8 -*-
-#
-# This file is part of INGInious. See the LICENSE and the COPYRIGHTS files for
-# more information about the licensing of this file.
-
 import os
-import re
 import yaml
-import datetime
 
-_scripts_path = '/task/student/scripts/'
-_build_configuration_filename = 'build.yaml'
-_step_configuration_filename = 'step_configuration.yaml'
+_default_scripts_dir = '/task/student/scripts/'
+_build_configuration_filename = os.path.join(_default_scripts_dir, 'build.yaml')
 
-def _load_config():
+def _load_build_config():
     """ Open existing config file """
-    with open(os.path.join(_scripts_path, _step_configuration_filename), 'r') as configuration_fd:
-        return yaml.safe_load(configuration_fd)
-
+    with open(_build_configuration_filename, 'r') as build_in:
+        return yaml.safe_load(build_in)
 
 def get_config(field_name):
     """" Returns the specified problem answer in the form 
          problem: problem id
          Returns string, or bytes if a file is loaded
     """
-    configuration = _load_config()
-    field_split = value_name.split(":")
-    step_configuration = configuration[field_split[0]]
-    if isinstance(step_configuration, dict) and "filename" in step_configuration and "value" in step_configuration:
+    configuration = _load_build_config()
+    field_split = field_name.split(":")
+    build_configuration = configuration[field_split[0]]
+    if isinstance(build_configuration, dict) and "filename" in build_configuration and "value" in build_configuration:
         if len(field_split) > 1 and field_split[1] == 'filename':
-            return step_configuration["filename"]
+            return build_configuration["filename"]
         else:
-            return open(step_configuration["value"], 'rb').read()
+            with open(build_configuration["value"], 'rb') as fin:
+                return fin.read()
     else:
-        return step_configuration
-
+        return build_configuration
--- containers/cychall-base/cychall_container_api/steps.py
+++ containers/cychall-base/cychall_container_api/steps.py
@@ -1,25 +1,37 @@
 import os
 import pwd
 import shutil
-
-import utils
-import flag
+import yaml
 
 import inginious_container_api.utils
 from inginious_container_api.run_types import run_types
 
+import utils
+import flag
+import config
 
-is_step_path = \
-    lambda path: os.path.basename(path).startswith("step")
+_step_configuration_filename = os.path.join(config._default_scripts_dir, '.__step.yaml')
 
+def _load_step_config():
+    with open(_step_configuration_filename, 'r') as step_in:
+        return yaml.safe_load(step_in)
 
-def count_steps(path="/task/student/scripts"):
-    return sum(is_step_path(p) for p in utils.get_directory_content(path))
-
-
-def add_sudoers_nopasswd(privileged_user, normal_user, command):
-    with open("/etc/sudoers.d/chellenge", "a") as sudoers:
-        sudoers.write(f"{normal_user} ALL=({privileged_user}) NOPASSWD: {command}\n")
+def get_config(field_name):
+    """" Returns the specified problem answer in the form 
+         problem: problem id
+         Returns string, or bytes if a file is loaded
+    """
+    configuration = _load_step_config()
+    field_split = field_name.split(":")
+    step_configuration = configuration[field_split[0]]
+    if isinstance(step_configuration, dict) and "filename" in step_configuration and "value" in step_configuration:
+        if len(field_split) > 1 and field_split[1] == 'filename':
+            return step_configuration["filename"]
+        else:
+            with open(step_configuration["value"], 'rb') as fin:
+                return fin.read()
+    else:
+        return step_configuration
 
 
 def fix_output_directory_permissions(path):
@@ -40,32 +52,13 @@ class StepGenerationError(Exception):
 
 class Step:
 
-    def __init__(self, name, general_input_folder, general_output_folder):
+    def __init__(self, name, configuration, general_output_folder):
         # get the current user name and scripts source and destination
         self._name = name
-        self._input_folder = None if general_input_folder is None else os.path.join(general_input_folder, name)
-        self._output_folder = None if general_output_folder is None else os.path.join(general_output_folder, name)
+        self._configuration = {} if configuration is None else configuration
+        self._step_folder = os.path.join(general_output_folder, name)
         self._user_infos = None
         self._setup_files = []
-        
-        if self._input_folder is not None:
-            # if there is a missing step, advertise
-            if not os.path.exists(self._input_folder):
-                raise StepGenerationError(f"{self._input_folder} folder does not exist.")
-
-            self._setup_files = [
-                os.path.join(self._output_folder, p)
-                for p in os.listdir(self._input_folder)
-            ]
-
-    def _copy_step_files(self):
-        """
-            Copy the sources to build the challenge
-        """
-        try:
-            shutil.copytree(self._input_folder, self._output_folder)
-        except shutil.Error:
-            raise StepGenerationError("Error while copying a step content.")
 
     def _create_associated_user(self):
         """
@@ -75,7 +68,7 @@ class Step:
         self._user_infos = pwd.getpwnam(self._name)
 
     def _detect_script_command(self, script_name):
-        script_path = os.path.join(self._output_folder, script_name)
+        script_path = os.path.join(self._step_folder, script_name)
 
         if os.path.exists(script_path):
             return [script_path]
@@ -89,32 +82,45 @@ class Step:
         if self._user_infos is None:
             return
         
-        os.chmod(self._output_folder, 0o700)
-        os.chown(self._output_folder, self._user_infos.pw_uid, 4242)
+        os.chmod(self._step_folder, 0o700)
+        os.chown(self._step_folder, self._user_infos.pw_uid, 4242)
 
-        for root, dirs, files in os.walk(self._output_folder):
+        for root, dirs, files in os.walk(self._step_folder):
             for momo in dirs + files:
                 fullpath = os.path.join(root, momo)
                 if fullpath not in self._setup_files:
                     os.chown(os.path.join(root, momo), self._user_infos.pw_uid, 4242)
 
     def _clean_files(self):
-        utils.remove_files(self._setup_files)
+        utils.remove_files(
+            os.path.join(self._step_folder, file)
+            for file in self._step_files
+        )
+
+    def _write_step_config(self):
+        with open(_step_configuration_filename, 'w') as step_out:
+            yaml.safe_dump(self._configuration, step_out)
 
     def build(self):
+        # if there is a missing step, advertise
+        if not os.path.exists(self._step_folder):
+            raise StepGenerationError(f"{self._step_folder} folder does not exist.")
+        self._step_files = os.listdir(self._step_folder)
+
         self._create_associated_user()
-        self._copy_step_files()
 
         setup_command = self._detect_script_command("setup")
         post_command = self._detect_script_command("post")
+
+        self._write_step_config()
         
         if setup_command is not None:
-            inginious_container_api.utils.execute_process(setup_command, user=utils.get_username(), cwd=self._output_folder)
+            inginious_container_api.utils.execute_process(setup_command, user=utils.get_username(), cwd=self._step_folder)
 
-        self._set_default_permissions()
+        # self._set_default_permissions()
 
         if post_command is not None:
-            inginious_container_api.utils.execute_process(post_command, user=utils.get_username(), cwd=self._output_folder)
+            inginious_container_api.utils.execute_process(post_command, user=utils.get_username(), cwd=self._step_folder)
 
         self._clean_files()
 
@@ -125,12 +131,12 @@ class EndStep(Step):
         super().__init__("end", None, general_output_folder)
 
     def build(self):
-        os.makedirs(self._output_folder, exist_ok=True)
+        os.makedirs(self._step_folder, exist_ok=True)
         self._create_associated_user()
 
         flag.write_flag(
             flag.generate_flag(),
-            os.path.join(self._output_folder, "flag")
+            os.path.join(self._step_folder, "flag")
         )
 
         self._set_default_permissions()
